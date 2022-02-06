eb4f516ddd116af13e4e8a2a73c0526a207947ee
Stavros Kounis <skounis@gmail.com>
2022-02-06 19:45:14 +0100

    feat(essentials) Import and use.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as essentials from 'sk-essentials';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sk-essentials-test-angular';
+  
+  constructor() {
+    const msg = essentials.merci();
+    alert(msg);
+  }
 }
